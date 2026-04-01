183f255ff3af108a58f7efd3288d0b4d1eacf945
Luyao Li <luyaoli@Luyaos-Mac-mini.local>
2026-03-31 22:28:23 -0700

    change config

--- cs336_systems/config.py
+++ cs336_systems/config.py
@@ -1,5 +1,11 @@
+# benchmark_config = {
+#     "name": "benchmark",
+#     "input_file": "/Users/luyaoli/code/cs336/assignment2-systems/cs336_systems/data/TinyStoriesV2-GPT4-train.txt",
+#     "encoding_output_dir": "/Users/luyaoli/code/cs336/assignment2-systems/cs336_systems/encoding",
+# }
+
 benchmark_config = {
     "name": "benchmark",
-    "input_file": "/Users/luyaoli/code/cs336/assignment2-systems/cs336_systems/data/TinyStoriesV2-GPT4-train.txt",
-    "encoding_output_dir": "/Users/luyaoli/code/cs336/assignment2-systems/cs336_systems/encoding",
+    "input_file": "/workspace/code/assignment2-systems/data/TinyStoriesV2-GPT4-train.txt",
+    "encoding_output_dir": "/workspace/code/assignment2-systems/cs336_systems/encoding",
 }
