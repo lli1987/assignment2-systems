d69ba50c70c2bb651944b95c2754b082ff8a56a7
Luyao Li <luyaoli@Luyaos-Mac-mini.local>
2026-04-19 16:08:43 -0700

    change config path

--- cs336_systems/config.py
+++ cs336_systems/config.py
@@ -13,7 +13,7 @@ local_benchmark_config = {
 
 benchmark_config = {
     "name": "benchmark",
-    "input_file": "/workspace/code/assignment2-systems/data/TinyStoriesV2-GPT4-train.txt",
+    "input_file": "/workspace/data/TinyStoriesV2-GPT4-train.txt",
     "encoding_output_dir": "/workspace/code/assignment2-systems/cs336_systems/encoding",
     "serde_output_dir": "/Users/luyaoli/code/cs336/assignment2-systems/cs336_systems/serde",
     "is_local": False,
