96a5b4ae9c9a2530d7d20a5efc054c82ac7474a8
Luyao Li <luyaoli@Luyaos-Mac-mini.local>
2026-04-24 16:01:25 -0700

    change commands

--- cs336_systems/setup.sh
+++ cs336_systems/setup.sh
@@ -1,8 +1,8 @@
 pip install uv
-apt update
-apt install vim
-apt install wget gnupg
+apt update -y
+apt install -y
+apt install -y wget gnupg
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/debian12/x86_64/cuda-keyring_1.1-1_all.deb
 dpkg -i cuda-keyring_1.1-1_all.deb
-apt install nsight-systems-2025.6.3
+apt install -y nsight-systems-2025.6.3
