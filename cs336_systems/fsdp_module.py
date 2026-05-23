15fb55b1f21d32e32452fc87a432274463ce4856
Luyao Li <luyaoli@Luyaos-Mac-mini.local>
2026-05-23 13:27:00 -0700

    add fsdp

--- cs336_systems/fsdp_module.py
+++ cs336_systems/fsdp_module.py
@@ -14,6 +14,7 @@ Key features:
 
 import torch
 import torch.distributed as dist
+from torch.nn.modules import Linear, Embedding
 
 
 class FSDP(torch.nn.Module):
@@ -21,41 +22,132 @@ class FSDP(torch.nn.Module):
     def __init__(self, module: torch.nn.Module, compute_dtype: torch.dtype | None = None):
         super().__init__()
         self.module = module
+        self.compute_dtype = compute_dtype
         self.handles = []
-        self._register_pre_forward_hooks()
+        self.rank = dist.get_rank()
+        self.world_size = dist.get_world_size()
+        self.param_metadata = dict()
+        self._broadcast_params()  # Ensure all ranks start with same params
+        self._shard_params()
+        self._register_forward_pre_hooks()
+        self._register_forward_hooks()
+        self._register_backward_pre_hooks()
         self._register_backward_hooks()
 
-    def _register_pre_forward_hooks(self):
-        for module in self.module.modules:
-            module.register_forward_pre_hook(self._make_pre_forward_hook())
+    def _broadcast_params(self):
+        """Broadcast all parameters from rank 0 to ensure consistency."""
+        for param in self.module.parameters():
+            dist.broadcast(param.data, src=0)
 
-    def _make_pre_forward_hook(self):
-        def hook(module: torch.nn.Module):
+    def _shard_params(self):
+        for module in self.module.modules():
+            if isinstance(module, (Linear, Embedding)):
+                for param in module.parameters(recurse=False):
+                    flatten = param.data.view(-1)
+                    shard_size = flatten.size(dim=0) // self.world_size
+                    local_shard_param_data = flatten[self.rank * shard_size : (self.rank + 1) * shard_size]
+                    self.param_metadata[id(param)] = param.data.shape
+                    param.data = local_shard_param_data
+
+    def _register_forward_pre_hooks(self):
+        for module in self.module.modules():
+            if isinstance(module, (Linear, Embedding)):
+                module.register_forward_pre_hook(self._make_forward_pre_hook())
+
+    def _register_backward_pre_hooks(self):
+        for module in self.module.modules():
+            if isinstance(module, (Linear, Embedding)):
+                module.register_full_backward_pre_hook(self._make_backward_pre_hook())
+
+    def _register_forward_hooks(self):
+        for module in self.module.modules():
+            if isinstance(module, (Linear, Embedding)):
+                module.register_forward_hook(self._make_forward_hook())
+
+    def _register_backward_hooks(self):
+        for module in self.module.modules():
+            if isinstance(module, (Linear, Embedding)):
+                # Sharded params: reduce-scatter
+                module.register_full_backward_hook(self._make_backward_hook())
+            elif list(module.parameters(recurse=False)):
+                # Replicated params: all-reduce
+                module.register_full_backward_hook(self._make_replicated_backward_hook())
+
+    def _make_forward_hook(self):
+        def hook(module: torch.nn.Module, input, output):
             for param in module.parameters(recurse=False):
-                gathered_param = []
-                dist.all_gather(gathered_param, param)
-                param.data = torch.concat(gathered_param, dim=0)
+                # Cast back to FP32 if we used compute_dtype
+                if self.compute_dtype is not None:
+                    param.data = param.data.to(torch.float32)
+
+                flatten = param.data.view(-1)
+                shard_size = flatten.size(dim=0) // self.world_size
+                param.data = flatten[self.rank * shard_size : (self.rank + 1) * shard_size]
 
         return hook
 
-    def _register_backward_hooks(self):
-        """
-        Register backward hooks on all parameters that require gradients.
+    def _make_backward_pre_hook(self):
+        def hook(module, grad_output):
+            for param in module.parameters(recurse=False):
+                flatten = param.data
+                gathered_param = [torch.empty_like(flatten) for _ in range(dist.get_world_size())]
+                dist.all_gather(gathered_param, flatten)
+                full_flat = torch.concat(gathered_param, dim=0)
+                param.data = full_flat.view(self.param_metadata[id(param)])
 
-        Each hook will be called when the gradient for that parameter is ready,
-        allowing us to launch the all-reduce operation immediately (overlapping
-        communication with backward computation).
-        """
-        for param in self.module.parameters():
-            if param.requires_grad:
-                param.register_post_accumulate_grad_hook(self._make_backward_hook())
+                # Cast to compute_dtype for backward pass if specified
+                if self.compute_dtype is not None:
+                    param.data = param.data.to(self.compute_dtype)
+
+        return hook
+
+    def _make_forward_pre_hook(self):
+        def hook(module: torch.nn.Module, input):
+            for param in module.parameters(recurse=False):
+                flatten = param.data
+                gathered_param = [torch.empty_like(flatten) for _ in range(dist.get_world_size())]
+                dist.all_gather(gathered_param, flatten)
+                full_flat = torch.concat(gathered_param, dim=0)
+                param.data = full_flat.view(self.param_metadata[id(param)])
+
+                # Cast to compute_dtype for forward pass if specified
+                if self.compute_dtype is not None:
+                    param.data = param.data.to(self.compute_dtype)
+
+        return hook
 
     def _make_backward_hook(self):
-        def hook(param: torch.Tensor):
-            gathered_param = param.grad
-            full_param_list = gathered_param.chunk(dist.get_world_size())
-            handle = dist.reduce_scatter(param.grad, full_param_list, dist.ReduceOp.AVG, async_op=True)
-            self.handles.append(handle)
+        def hook(module: torch.nn.Module, grad_in, grad_out):
+            for param in module.parameters(recurse=False):
+                if param.grad is None:
+                    continue
+
+                # Cast gradient to FP32 if we used compute_dtype
+                if self.compute_dtype is not None:
+                    param.grad = param.grad.to(torch.float32)
+
+                # Restore param.data to FP32 before re-sharding
+                if self.compute_dtype is not None:
+                    param.data = param.data.to(torch.float32)
+
+                flatten_weights = param.data.view(-1)
+                shard_size = flatten_weights.size(dim=0) // self.world_size
+                param.data = flatten_weights[self.rank * shard_size : (self.rank + 1) * shard_size]
+
+                flatten = param.grad.view(-1)
+                full_param_list = list(torch.chunk(flatten, self.world_size))
+                grad_shard = torch.empty(shard_size, dtype=param.grad.dtype, device=param.grad.device)
+                dist.reduce_scatter(grad_shard, full_param_list, dist.ReduceOp.AVG)
+                param.grad = grad_shard
+
+        return hook
+
+    def _make_replicated_backward_hook(self):
+        """All-reduce gradients for replicated (non-sharded) parameters."""
+        def hook(module: torch.nn.Module, grad_in, grad_out):
+            for param in module.parameters(recurse=False):
+                if param.grad is not None:
+                    dist.all_reduce(param.grad, op=dist.ReduceOp.AVG)
 
         return hook
 
--- tests/adapters.py
+++ tests/adapters.py
@@ -88,8 +88,9 @@ def get_fsdp(module: torch.nn.Module, compute_dtype: torch.dtype | None = None) 
     Returns:
         Instance of an FSDP class.
     """
-    # For example: return FSDP(module, compute_dtype=compute_dtype)
-    raise NotImplementedError
+    from cs336_systems.fsdp_module import FSDP
+
+    return FSDP(module, compute_dtype=compute_dtype)
 
 
 def fsdp_on_after_backward(fsdp_model: torch.nn.Module, optimizer: torch.optim.Optimizer):
@@ -104,7 +105,8 @@ def fsdp_on_after_backward(fsdp_model: torch.nn.Module, optimizer: torch.optim.O
             Optimizer being used with the FSDP-wrapped model.
     """
     # For example: fsdp_model.finish_gradient_synchronization()
-    raise NotImplementedError
+
+    fsdp_model.finish_gradient_synchronization()
 
 
 def fsdp_gather_full_params(fsdp_model: torch.nn.Module) -> dict[str, torch.Tensor]:
@@ -118,7 +120,25 @@ def fsdp_gather_full_params(fsdp_model: torch.nn.Module) -> dict[str, torch.Tens
     Returns:
         State dictionary mapping parameter names to full (unsharded) tensors.
     """
-    raise NotImplementedError
+    import torch.distributed as dist
+
+    gather_full_params = dict()
+
+    for name, param in fsdp_model.module.named_parameters():
+        # Check if this parameter is sharded (will be in fsdp_model.param_metadata)
+        if id(param) in fsdp_model.param_metadata:
+            # Sharded parameter: all-gather to reconstruct full parameter
+            flatten = param.data
+            gathered_param = [torch.empty_like(flatten) for _ in range(dist.get_world_size())]
+            dist.all_gather(gathered_param, flatten)
+            full_flat = torch.concat(gathered_param, dim=0)
+            # Reshape to original shape
+            gather_full_params[name] = full_flat.view(fsdp_model.param_metadata[id(param)])
+        else:
+            # Replicated parameter: already has full data
+            gather_full_params[name] = param.data
+
+    return gather_full_params
 
 
 def get_sharded_optimizer(params, optimizer_cls: type[torch.optim.Optimizer], **kwargs) -> torch.optim.Optimizer:
